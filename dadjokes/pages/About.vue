70c583b175904883e2753a8394a30f2293c41921
CloudMunk <munkeyten@protonmail.com>
2019-11-04 13:42:02 +0100

    Render + server + API
    API calls with renders and express.js server set-up

---
+++ dadjokes/pages/About.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>About Dad Jokes</h1>
+        <p>This is an app that displays corny dad jokes.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About the app',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Best place fo corny dad jokes'
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ dadjokes/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h2>
+        Welcome to the best corny dad jokes app on the web
+    </h2>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  head() {
+      return {
+          title: 'Welcome to Dad Jokes',
+          meta: [
+              {
+                  hid: 'description',
+                  name: 'description',
+                  content: 'Best place fo corny dad jokes'
+              }
+          ]
+      }
+  }
+}
+</script>
+
