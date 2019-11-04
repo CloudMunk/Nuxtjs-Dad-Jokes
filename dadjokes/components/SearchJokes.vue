272605314ed064cb0afece7b6251eadbf473cdca
CloudMunk <munkeyten@protonmail.com>
2019-11-04 21:52:28 +0100

    Search bar and display singe joke/id
    Search bar and display singe joke/id

---
+++ dadjokes/components/SearchJokes.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <form @submit.prevent="onSubmit">
+            <input type="text" v-model="text" placeholder="Search jokes">
+            <input type="submit" value="Seacr Jokes">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search Jokes',
+    data() {
+        return {
+            text: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            this.$emit('search/text', this.text)
+            this.text = ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
